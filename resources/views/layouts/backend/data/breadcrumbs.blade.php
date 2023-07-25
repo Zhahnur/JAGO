d2ffb140d325a2a548cc3f5981ef332b4ff9cf2b
Dewi Nahrisa Amelia <dewinahrisa.amel@gmail.com>
2023-07-25 14:39:20 +0700

    Update Risa

--- resources/views/layouts/backend/data/breadcrumbs.blade.php
+++ resources/views/layouts/backend/data/breadcrumbs.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-12 col-md-6 order-md-1 order-last">
             <h3 class="text-capitalize">{{ request()->segment(3) ?? request()->segment(2)  }}</h3>
-            <p class="text-subtitle text-muted">Navbar will appear on the top of the page.</p>
+            <p class="text-subtitle text-muted">Ada navbar diatas.</p>
         </div>
         <div class="col-12 col-md-6 order-md-2 order-first">
             <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
