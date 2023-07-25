d5a02727a14d1c077649c9ee00b00e148175167b
Matkids <cogantortorridho@gmail.com>
2023-07-25 16:23:41 +0700

    updatev2

--- resources/views/frontend/voting/index.blade.php
+++ resources/views/frontend/voting/index.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <tittle>JAGO</tittle>
     <title>{{ $app_name }}</title>
     <link rel="stylesheet" href="{{ asset('mazer') }}/assets/css/main/app.css" />
     <link rel="shortcut icon" href="{{ asset('mazer') }}/assets/images/logo/favicon.svg" type="image/x-icon">
