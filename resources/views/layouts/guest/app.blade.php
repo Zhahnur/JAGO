8f46a2057d64c74104010166b5c3c06bdac0bf52
Matkids <cogantortorridho@gmail.com>
2023-07-25 15:26:57 +0700

    update rahmat

--- resources/views/layouts/guest/app.blade.php
+++ resources/views/layouts/guest/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JAGO</title>
     <title>{{ $app_name }}</title>
     <link rel="stylesheet" href="{{ asset('mazer') }}/assets/css/main/app.css">
     <link rel="stylesheet" href="{{ asset('mazer') }}/assets/css/pages/auth.css">
